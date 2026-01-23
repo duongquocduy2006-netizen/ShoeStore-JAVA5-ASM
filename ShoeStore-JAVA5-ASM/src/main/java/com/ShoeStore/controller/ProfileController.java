191907cdcff8bc387b82fceb47bc52677573cc94
duongquocduy2006-dev <256564288+duongquocduy2006-netizen@users.noreply.github.com>
2026-01-24 00:05:32 +0700

    profile, còn lỗi

---
+++ ShoeStore-JAVA5-ASM/src/main/java/com/ShoeStore/controller/ProfileController.java
@@ -0,0 +1,17 @@
+package com.ShoeStore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class ProfileController {
+    @GetMapping("/profile")
+    public String viewProfile() { return "client/profile"; }
+
+    @GetMapping("/orders")
+    public String viewOrders() { return "client/orders"; }
+
+    @GetMapping("/change-password")
+    public String viewChangePassword() { return "client/change-password"; }
+}
