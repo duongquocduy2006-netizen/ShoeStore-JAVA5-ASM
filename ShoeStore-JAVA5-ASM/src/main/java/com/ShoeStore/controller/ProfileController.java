1cca76ab880ba98f63bdddf1d87f82d3626a3cf8
duongquocduy2006-dev <256564288+duongquocduy2006-netizen@users.noreply.github.com>
2026-01-24 10:33:55 +0700

    ghj

--- ShoeStore-JAVA5-ASM/src/main/java/com/ShoeStore/controller/ProfileController.java
+++ ShoeStore-JAVA5-ASM/src/main/java/com/ShoeStore/controller/ProfileController.java
@@ -6,12 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 
 public class ProfileController {
-    @GetMapping("/profile")
-    public String viewProfile() { return "client/profile"; }
+	@GetMapping("/profile")
+	public String viewProfile() {
+		return "client/profile";
+	}
 
-    @GetMapping("/orders")
-    public String viewOrders() { return "client/orders"; }
+	@GetMapping("/orders")
+	public String viewOrders() {
+		return "client/orders";
+	}
 
-    @GetMapping("/change-password")
-    public String viewChangePassword() { return "client/change-password"; }
+	@GetMapping("/change-password")
+	public String viewChangePassword() {
+		return "client/change-password";
+	}
 }
