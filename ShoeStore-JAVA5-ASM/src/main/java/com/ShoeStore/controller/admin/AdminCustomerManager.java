5faf548403f4d724dc3b008be7ee95605183f71a
duongquocduy2006-dev <256564288+duongquocduy2006-netizen@users.noreply.github.com>
2026-02-24 22:35:08 +0700

    đ

--- ShoeStore-JAVA5-ASM/src/main/java/com/ShoeStore/controller/admin/AdminCustomerManager.java
+++ ShoeStore-JAVA5-ASM/src/main/java/com/ShoeStore/controller/admin/AdminCustomerManager.java
@@ -1,13 +1,21 @@
 package com.ShoeStore.controller.admin;
 
+import com.ShoeStore.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminCustomerManager {
-	
-	@GetMapping("/admin/customers")
-	public String dashBoard() {
-		return "admin/customers";
-	}
+    
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/admin/customers")
+    public String dashBoard(Model model) {
+        // Đẩy danh sách khách hàng sang giao diện Thymeleaf
+        model.addAttribute("customers", customerService.getAllCustomers());
+        return "admin/customers";
+    }
 }
